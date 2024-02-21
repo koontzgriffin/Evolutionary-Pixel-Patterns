a39abfe721c047ed786927f1c726a7287b32364d
Griffin <griffinkoontz@Griffins-MacBook-Pro-2.local>
2024-02-21 17:45:30 -0500

    inactive neighborhood cookie setup

--- src/main.js
+++ src/main.js
@@ -29,11 +29,7 @@ let showBorders = true;
 // Constraint Params ///////
 ////////////////////////////
 
-const defaultValidInactiveNeighborhoods  = ['000101000', '000101001', '000101100', '000101101', '000101111', '001100001', '001100101', '001100110', '001100111', '001101000', '001101001', '001101100', '001101101', '001101111', '010000010', '010000011', '010000101', '010000110', '010000111', '011000010', '011000011', '011000101', '011000110', '011000111', '011001101', '100001111', '100001101', '100001100', '100001011', '100101000', '100101001', '100101101', '100101100', '100101111', '101000010', '101000011', '101000101', '101000110', '101000111', '101001011', '101001100', '101001101', '101001111', '101100001', '101100101', '101100110', '101100111', '101101000', '101101001', '101101100', '101101101', '101101111', '110000010', '110000011', '110000101', '110000110', '110000111', '110100001', '110100101', '110100110', '110100111', '111000010', '111000011', '111000101', '111000110', '111000111', '111001011', '111001100', '111001101', '111001111', '111100001', '111100101', '111101000', '111100111', '111100110', '111101001', '111101101', '111101100', '011001111', '011001011', '011001100'];
-let vaildInactiveNeighborhoods = defaultValidInactiveNeighborhoods;
-
-const defaultValidActiveNeighborhoods  = [];
-let vaildActiveNeighborhoods = defaultValidInactiveNeighborhoods;
+let vaildInactiveNeighborhoods = getAllowedNeighborhoodsCookie();
 
 ////////////////////////////
 // Genetic Algo Params  ////
@@ -174,8 +170,11 @@ function geneticAlgorithm(rows, columns, populationSize, maxIterations, constrai
                 runNextIteration(); // Run the next iteration recursively
             }, 0);
         } else {
+            const bestIndividual = population.getBestIndividual();
             toggleNoSolutionError(true);
             changeCount(currentIteration);
+            drawGridCallback(bestIndividual, showBorders);
+            mainGrid = bestIndividual;
             console.log(bestIndividual);
         }
     }
@@ -248,7 +247,14 @@ function checkConstraintsHandler(){
         for(let x = 0; x < columns; x++){
             // run each constraint on each cell
             for(let constraint of constraints){
-                num_conflicts += constraint.evaluate(mainGrid.getCell(x, y), mainGrid);
+                let deduction = constraint.evaluate(mainGrid.getCell(x, y), mainGrid);
+                num_conflicts += deduction;
+                if(deduction > 0){
+                    ctx.fillStyle = "#D10000";
+                    const x_canvas = x * cellSize;
+                    const y_canvas = y * cellSize;
+                    ctx.strokeRect(x_canvas, y_canvas, cellSize, cellSize);
+                }
             }
         }
     }
@@ -257,6 +263,7 @@ function checkConstraintsHandler(){
 
 function generateHandler(){
     console.log("generating with genetic algorithm...");
+    vaildInactiveNeighborhoods = getAllowedNeighborhoodsCookie();
     const result = geneticAlgorithm(rows, columns, populationSize, maxIterations, constraints, drawGrid);
     //mainGrid = result;
     //drawGrid(mainGrid, showBorders);
